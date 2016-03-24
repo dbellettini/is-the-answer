83039896b72dd3eef5dbb809ecf0bfc3415afdfc
Davide Bellettini <davide@bellettini.eu>
2016-03-24 14:35:11 +0100

    Simple implementation

---
+++ test/answer.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+describe('is-the-answer', function () {
+    var isTheAnswer = require('..');
+
+    it('should say that 42 IS the answer', function(done) {
+        expect(isTheAnswer(42)).to.be.true;
+    });
+
+    it('should say that 41 IS NOT the answer', function(done) {
+        expect(isTheAnswer(41)).to.be.false;
+    });
+});
